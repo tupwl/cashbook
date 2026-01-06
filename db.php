335021117b17124e90d1b758f02b8da78a9b916d
tupwl <praphanitwangkaew@gmail.com>
2026-01-06 11:31:51 +0700

    db

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-$host = $_ENV['MYSQLHOST'];
-$user = $_ENV['MYSQLUSER'];
-$pass = $_ENV['MYSQLPASSWORD'];
-$db   = $_ENV['MYSQLDATABASE'];
-$port = $_ENV['MYSQLPORT'];
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$name = getenv('DB_NAME');
+$port = getenv('DB_PORT') ?: 3306;
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
-$conn->set_charset("utf8");
+$conn = new mysqli($host, $user, $pass, $name, $port);
 
 if ($conn->connect_error) {
-    die("DB Error");
+    die("DB Connection failed: " . $conn->connect_error);
 }
+$conn->set_charset("utf8mb4");
